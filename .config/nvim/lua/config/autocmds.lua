770f1d55f773d54b75b8084bc366723298824af3
Artem Harlamenko <4.harlamenko@gmail.com>
2024-08-29 16:15:27 +0300

    fix typo

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- save all on foces lost
+-- save all on focus lost
 vim.api.nvim_create_autocmd({ "FocusLost" }, {
   pattern = { "*" },
   command = "silent! wall",
